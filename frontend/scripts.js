c6473918a639dc0e5502201a9096fda1c1a2a3f0
Fellipe Neves <fellipe_emanoel@univem.edu.br>
2020-02-18 19:19:53 -0300

    Primeiro Commit: Frontend Finalizado

---
+++ frontend/scripts.js
@@ -0,0 +1,11 @@
+
+//Pega o botao e adiciona um evento a ele
+document.querySelector('header button').addEventListener("click", function(){
+    event.preventDefault();
+
+    document.querySelector('.form').classList.toggle("hide");        //Se tiver a classe hide tira, caso contrario, ele adiciona
+});
+
+document.querySelector('section form button').addEventListener("click", function(){
+    event.preventDefault();
+});
